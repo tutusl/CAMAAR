23dc68f699e84939ba5f4080ee4f694443218b0b
evolpi <evolpi@cgee.org.br>
2025-02-03 16:33:48 -0300

    modified:   app/Gemfile.lock 	new file:   app/app/controllers/templates_controller.rb 	modified:   app/app/models/questao.rb     modified:   app/app/models/template.rb 	new file:   app/app/views/layouts/templates.html.erb 	new file:   app/app/views/templates/edit.html.erb 	new file:   app/app/views/templates/index.html.erb 	new file:   app/app/views/templates/new.html.erb 	new file:   app/components/question_fields_component.rb 	new file:   app/components/template_card_component.rb 	new file:   app/components/template_form_component.rb 	modified:   app/config/routes.rb 	modified:   app/db/schema.rb

--- app/app/models/template.rb
+++ app/app/models/template.rb
@@ -1,9 +1,9 @@
+# app/models/template.rb
 class Template < ApplicationRecord
-    has_many :formularios
-    has_many :questoes
-    
-    validates :idTemplate, presence: true, uniqueness: true
-    validates :nomeTemplate, presence: true
-    validates :dataCriacao, presence: true
-    validates :semestre, presence: true
-  end
+  has_many :formularios
+  has_many :questoes
+  validates :idTemplate, presence: true, uniqueness: true
+  validates :nomeTemplate, presence: true
+  validates :dataCriacao, presence: true
+  validates :semestre, presence: true
+end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,12 +1,12 @@
+# config/routes.rb
 Rails.application.routes.draw do
   root to: "sessions#new"
   get "importa_dados/import"
-  # post "importa_dados/import"
   resources :usuarios
-  resources :sessions, only: [ :new, :create, :destroy ]
-  resources :respostas, only: [ :index, :show ]
-
-
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :respostas, only: [:index, :show]
+  resources :templates # Adicionando esta linha
+  
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/emails"
   end
