4590831937d9f0843af72b21189772ae5f325e2b
henrique-givisiez <lucogds@gmail.com>
2025-02-05 16:47:51 -0300

    email alterar senha implementado

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,8 +1,19 @@
 # config/routes.rb
 Rails.application.routes.draw do
   root to: "sessions#new"
+  get '/usuarios/alterar_senha', to: 'usuarios#alterar_senha_form', as: 'alterar_senha_form'
   resources :importadados, only: [:new, :create]
+<<<<<<< Updated upstream
   resources :usuarios
+=======
+  resources :usuarios do
+    member do
+      get :edit
+      patch :update
+      post :alterar_senha # Adicionando esta linha
+    end
+  end
+>>>>>>> Stashed changes
   resources :sessions, only: [:new, :create, :destroy]
   resources :respostas, only: [:index, :show]
   resources :formularios
