3d6df56d110a7f29cf0eb238688a6da6f0d56095
tutusl <tutuslopes@gmail.com>
2025-02-17 20:06:56 -0300

    added funções em formularios controller

--- app/app/controllers/formularios_controller.rb
+++ app/app/controllers/formularios_controller.rb
@@ -1,17 +1,38 @@
 # frozen_string_literal: true
 
 class FormulariosController < ApplicationController
-  def index; end
 
-  def show; end
+  def index
+    @formularios = Formulario.all
+  end
 
-  def new; end
+  def new
+    @templates = Template.all # Carrega todos os templates disponíveis
+    @formulario = Formulario.new
+  end
 
-  def create; end
+  def create
+    @formulario = Formulario.new(formulario_params)
+    @formulario.dataCriacao = Time.current
 
-  def edit; end
+    if @formulario.save
+      redirect_to formularios_path, notice: 'Formulário criado com sucesso!'
+    else
+      @templates = Template.all
+      render :new
+    end
+  end
 
-  def update; end
+  def show
+    @formulario = Formulario.find(params[:id])
+    @template = @formulario.template
+    @questaos = @template.questaos
+    @respostas = @formulario.respostas
+  end
 
-  def destroy; end
+  private
+
+  def formulario_params
+    params.require(:formulario).permit(:template_id, :tipoDestinatario)
+  end
 end
