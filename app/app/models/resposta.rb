9d2067a09ab0c093748746036a42d7cda1f1e588
tutusl <tutuslopes@gmail.com>
2025-02-17 20:05:31 -0300

    fix: corrigindo model respostas

--- app/app/models/resposta.rb
+++ app/app/models/resposta.rb
@@ -5,7 +5,6 @@ class Resposta < ApplicationRecord
   belongs_to :formulario
   belongs_to :questao
 
-  validates :idResposta, presence: true, uniqueness: true
   validates :dataResposta, presence: true
   validates :valorResposta, presence: true
 end
